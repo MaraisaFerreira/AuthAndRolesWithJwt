442b66f85152189272e6be9fda72787a5b10a385
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-04-25 13:10:01 -0300

    init

---
+++ src/main/java/study/mf/AuthAndRolesWithJwt/AuthAndRolesWithJwtApplication.java
@@ -0,0 +1,13 @@
+package study.mf.AuthAndRolesWithJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAndRolesWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAndRolesWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/mf/AuthAndRolesWithJwt/AuthAndRolesWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package study.mf.AuthAndRolesWithJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAndRolesWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
