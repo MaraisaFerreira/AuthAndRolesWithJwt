adc6323a1a17ee29b361ae8089fd936a6b7caefe
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-04-26 11:35:15 -0300

    feat. validate email type

--- src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/dto/request/RegisterRequestDto.java
+++ src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/dto/request/RegisterRequestDto.java
@@ -1,13 +1,19 @@
 package study.mf.AuthAndRolesWithJwt.infra.security.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import study.mf.AuthAndRolesWithJwt.infra.security.entity.enums.Roles;
 
 public record RegisterRequestDto(
-        @NotBlank(message = "FirstName cannot be null or empty.") String firstName,
-        @NotBlank(message = "LastName cannot be null or empty.") String lastName,
-        @NotBlank(message = "Email cannot be null or empty.") String email,
-        @NotNull(message = "Role cannot be null or empty") Roles role
+        @NotBlank(message = "FirstName cannot be null or empty.")
+        String firstName,
+        @NotBlank(message = "LastName cannot be null or empty.")
+        String lastName,
+        @Email(message = "Invalid email format", regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+        @NotBlank(message = "Email cannot be null or empty.")
+        String email,
+        @NotNull(message = "Role cannot be null or empty")
+        Roles role
 ) {
 }
