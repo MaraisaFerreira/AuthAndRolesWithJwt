290e995093e1e936f2a41db43e78915fcc5241e6
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-04-26 12:33:02 -0300

    feat. authenticationManager bean created

--- src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/config/SecurityConfig.java
+++ src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,5 +34,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+        return configuration.getAuthenticationManager();
     }
 }
