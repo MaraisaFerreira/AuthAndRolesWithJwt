1dfd9d1c11c93f9fd288516f6c43931905a49a30
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-04-27 11:44:04 -0300

    removed unused imports

--- src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/repository/UserRepository.java
+++ src/main/java/study/mf/AuthAndRolesWithJwt/infra/security/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package study.mf.AuthAndRolesWithJwt.infra.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.mf.AuthAndRolesWithJwt.infra.security.entity.User;
+import study.mf.AuthAndRolesWithJwt.infra.security.entity.enums.Roles;
 
 import java.util.Optional;
 import java.util.UUID;
